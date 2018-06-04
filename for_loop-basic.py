9eb619cb3761f3d517552d9f818649a5925157f6
Evan Hobbs <evan.hobbs1988@gmail.com>
2018-06-04 15:42:07 -0500

    Add files via upload

---
+++ for_loop-basic.py
@@ -0,0 +1,39 @@
+#basic
+    for i in range(0, 150):
+    print(i)
+    def printAndCount():
+  counter = 0
+  for i in range(0, 150):
+    counter += 1
+    print i
+  print counter
+#Multiples of Five
+    def multiples(5,1000000):
+    for i in range(5,1000000*5):
+    print(i)
+#Counting, the Dojo Way
+def multiplesOfFive():
+  count = 5
+  while count <= 1000000:
+    print count
+    count += 5
+#Whoa. That Sucker's Huge
+  sum = 0
+  for i in range(0, 500000):
+    if i % 2 != 0:
+      sum += i
+  print i
+#Count for 4
+  start = 2018
+  while start > 0:
+    if start % 2 == 0:
+      print start
+    start -= 4
+#Flexible Countdown
+def flexCount(lowNum, highNum, mult):
+  for i in range(highNum, lowNum, -mult):
+    print i
+#predict output
+1.error i isnt defined
+2.error i isnt defined
+3.error i isnt defined
